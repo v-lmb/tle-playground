886965e85550f893f11e332af2d9e00e0647081d
v-lmb <virginie.amamelia@gmail.com>
2026-04-30 22:07:39 +0200

    Feat: add satellite storage with SQLAlchemy and upsert logic

--- ingest.py
+++ ingest.py
@@ -1,6 +1,10 @@
 import requests
 from dotenv import load_dotenv
-import os
+from sqlalchemy import select
+from datetime import datetime, timezone
+
+from app.database import init_db, SessionLocal
+from app.models import Satellite
 
 # Pour charger les variables du fichier . env
 load_dotenv()
@@ -39,12 +43,59 @@ def fetch_tle():
             "line1": line1,
             "line2": line2
         })
-        return satellites
+    return satellites
+
+
+def save_satellites(satellites):
+    """
+    Sauvegarde une liste de satellites en base (upsert)
+    Args:
+        satellites
+    """
+    # ouvre une session - espace de travail
+    session = SessionLocal()
+
+    try:
+        inserted = 0
+        updated = 0
+
+        for sat in satellites:
+            # chercher si le satellite existe déjà en base
+            existing = session.execute(
+                select(Satellite).where(Satellite.name == sat["name"])
+            ).scalar_one_or_none()
+
+            if existing is None:
+                # n'existe pas -> créer le nouvel objet et l'ajoute
+                new_sat = Satellite(
+                    name=sat["name"],
+                    line1=sat["line1"],
+                    line2=sat["line2"]
+                )
+                session.add(new_sat)
+                inserted += 1
+            else:
+                # si existe déjà -> mise à jour de ses données
+                existing.line1 = sat["line1"]
+                existing.line2 = sat["line2"]
+                existing.updated_at = datetime.now(timezone.utc)
+                updated += 1
+
+        # écrit tout en base d'un coup
+        session.commit()
+        print(f"Ingestion terminée : {inserted} insérés, {updated} mis à jour.")
+
+    except Exception as e:
+        # en cas d'erreur, tout est annulé, rien n'est écrit
+        session.rollback()
+        print(f"Erreur : {e}")
+
+    finally:
+        # on ferme toujours la session
+        session.close()
 
 
 if __name__ == "__main__":
+    init_db()
     satellites = fetch_tle()
-
-    # Affiche les 3 premiers pour vérifier
-    for sat in satellites[:3]:
-        print(sat)
+    save_satellites(satellites)
